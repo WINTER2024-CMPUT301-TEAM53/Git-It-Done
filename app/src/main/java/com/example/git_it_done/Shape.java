3ce9b17723f170653f5a8e6d03a125c0c399a90f
jgranoski <jgranosk@ualberta.ca>
2024-02-01 18:50:51 -0700

    Added Color

--- app/src/main/java/com/example/git_it_done/Shape.java
+++ app/src/main/java/com/example/git_it_done/Shape.java
@@ -3,5 +3,9 @@ package com.example.git_it_done;
 public abstract class Shape {
     private int x;
     private int y;
+<<<<<<< HEAD
     String color = "green";
+=======
+    String color = "blue";
+>>>>>>> 32be92d (Added Color)
 }
